21e123d1925292c2b1e0952c723e90d1390713a8
RuyHa <code.ruyha@gmail.com>
2023-03-28 10:24:08 +0900

    [fix] Storyboard 제거

---
+++ SaveUs/SaveUs/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  SaveUs
+//
+//  Created by Ruyha on 2023/03/28.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
