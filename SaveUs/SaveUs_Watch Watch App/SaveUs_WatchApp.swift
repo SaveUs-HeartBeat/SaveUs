cc85569f46017318aa2aea2c80722a2771ab6618
RuyHa <code.ruyha@gmail.com>
2023-05-15 21:09:27 +0900

    [feat] addWatchApp

---
+++ SaveUs/SaveUs_Watch Watch App/SaveUs_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveUs_WatchApp.swift
+//  SaveUs_Watch Watch App
+//
+//  Created by Ruyha on 2023/05/15.
+//
+
+import SwiftUI
+
+@main
+struct SaveUs_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
